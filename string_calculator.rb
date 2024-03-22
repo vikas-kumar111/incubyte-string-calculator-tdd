0edd84bfadb496f48534fd71dabc108630803807
vikas-kumar111 <vikas_kr@live.com>
2024-03-22 21:07:36 +0530

    Refactor: seprate string sum logic to another function

--- string_calculator.rb
+++ string_calculator.rb
@@ -2,10 +2,12 @@ def add(number_strings)
   return nil unless number_strings.is_a?(Array)
 
   number_strings.map do |number_string|
-    if number_string.is_a?(String)
-      number_string.split(',').map(&:to_i).sum
-    else
-      nil
-    end
+    add_string(number_string)
   end
+end
+
+def add_string(string)
+  return nil unless string.is_a?(String)
+
+  string.split(',').map(&:to_i).sum
 end
