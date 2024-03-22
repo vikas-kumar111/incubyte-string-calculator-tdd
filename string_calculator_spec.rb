a7b14da405f6069d9dd367c4b935e53029b1168b
vikas-kumar111 <vikas_kr@live.com>
2024-03-22 20:58:47 +0530

    Add: spec for input validation

--- string_calculator_spec.rb
+++ string_calculator_spec.rb
@@ -12,4 +12,16 @@ RSpec.describe '#add' do
   it 'returns the sum of numbers for comma-separated strings' do
     expect(add(['', '1', '1,5'])).to eq([0, 1, 6])
   end
+
+  it 'returns nil if input is not array' do
+    expect(add('')).to eq(nil)
+  end
+
+  it 'returns array nil if input is not an array of strings' do
+    expect(add([1, 2, 3])).to eq([nil, nil, nil])
+  end
+
+  it 'returns nil for each item in array which is not valid' do
+    expect(add(['', '1', '1,5', 1, 'a'])).to eq([0, 1, 6, nil, 0])
+  end
 end
