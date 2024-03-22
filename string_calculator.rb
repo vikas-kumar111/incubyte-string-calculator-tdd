224bc754ce7bb174e37f19d105911982aa21eac1
vikas-kumar111 <vikas_kr@live.com>
2024-03-22 21:01:08 +0530

    Update: Add function

--- string_calculator.rb
+++ string_calculator.rb
@@ -1,5 +1,11 @@
 def add(number_strings)
+  return nil unless number_strings.is_a?(Array)
+
   number_strings.map do |number_string|
-    number_string.split(',').map(&:to_i).sum
+    if number_string.is_a?(String)
+      number_string.split(',').map(&:to_i).sum
+    else
+      nil
+    end
   end
 end
